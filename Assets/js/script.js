5ad04aad928ba0741fb549c2efcf96b2dc3bf889
MorningSol <98545619+MorningSol@users.noreply.github.com>
2022-03-06 14:28:53 -0500

    added loop for password generator

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,3 +1,70 @@
+// var arrays
+
+var lowerCaseAlphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var upperCaseAlphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var numbers = ["0","1","2","3","4","5","6","7","8","9"]
+var specialCharacters =  ["!", '"', "'", "@", "#", "$", "%", "^", "&", "*", "(", ")", "[", "]", "{", "}", "+", ".", "`","^", "~", "|", "<", ">", "+", "=", "-", "_", ",", "/", ";", ":", "?"]
+
+
+var generatePassword = function() {
+
+  var numberChoice = window.prompt ("Please enter your the number of characters you would like your password to have.  Must be atleast 8 characters and no less than 128 ");
+  numberChoice = parseInt(numberChoice);
+  var passwordString = ""
+    
+  if (numberChoice >=8 && numberChoice <=128){
+    
+    var lowerCase = window.confirm ("Would you like lowercase letters in your password?");
+    
+    var upperCase = window.confirm ("Would you like uppercase letters in your password?");
+  
+    var NumericValues = window.confirm("Would you like numeric values in your password?");
+
+    var specialCharacterChoice = window.confirm ("Would you like special characters in your password?");
+
+    for(var i = 1; i <= numberChoice;) {
+      if(lowerCase && i <= numberChoice) {
+        passwordString += lowerCaseAlphabet[Math.floor(Math.random() * (lowerCaseAlphabet.length - 1))];
+        i++;
+      }
+      else {
+        continue
+      }
+  
+      if(upperCase && i <= numberChoice) {
+        passwordString += upperCaseAlphabet[Math.floor(Math.random() * (upperCaseAlphabet.length - 1))];
+        i++;
+      }
+      else {
+        continue
+      }
+  
+      if(NumericValues && i <= numberChoice) {
+        passwordString += numbers[Math.floor(Math.random() * (numbers.length - 1))];
+        i++;
+      }
+      else {
+        continue
+      }
+  
+      if(specialCharacterChoice && i <= numberChoice){
+        passwordString += specialCharacters[Math.floor(Math.random() * (specialCharacters.length - 1))];
+        i++;    
+      }
+      else {
+      continue
+      } 
+    }
+  }
+  else {
+      window.alert("You need to provide a value between 8 and 128. Please try again.");
+      writePassword()
+  }
+   return passwordString
+}
+
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -9,71 +76,6 @@ function writePassword() {
   passwordText.value = password;
 
 }
-// var arrays
-
-var lowerCaseAlphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var upperCaseAlphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-var numbers = [0,1,2,3,4,5,6,7,8,9]
-var specialCharacter =  ["!", '"', "'", "@", "#", "$", "%", "^", "&", "*", "(", ")", "[", "]", "{", "}", "+", ".", "`","^", "~", "|", "<", ">", "+", "=", "-", "_", ",", "/", ";", ":", "?"]
-
-
-// prompt for length of password
-
-var setParameters = function() {
-  var numberChoice = window.prompt ("Please enter your the number of characters you would like your password to have.  Must be atleast 8 characters and no less than 128 ");
-  numberChoice = parseInt(numberChoice);
-  console.log(numberChoice);
-    
-  if (numberChoice >=8 && numberChoice <=128){
-    
-    var lowerCase = window.confirm ("Would you like lowercase letters in your password?")
-    console.log(lowerCase)
-
-    var upperCase = window.confirm ("Would you like uppercase letters in your password?")
-    console.log(upperCase)
-
-    var specialCharacterChoice = window.confirm ("Would you like special characters in your password?")
-    console.log(specialCharacterChoice)
-  }
-
-  else {
-      window.alert("You need to provide a value between 8 and 128. Please try again.");
-      setParameters()
-  }
-}
 
   
-
-// minimum length 8 characters and maximum length of 128
-
-//prompted for lowercase yes or no
-//return said value
-//prompted for uppercase yes or no
-//return said value
-//prompted for numeric characters yes or no
-//return said value
-//prompted for special characters yes or no
-//return said value
-
-// if accepted, include variable in password
-
-//generate password randomly but with at least one of each variable accepted.
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
